9d07cee036d742894bd3fad496144a37636fb1f1
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-04-04 18:36:08 +0800

    fix: remove unused code

--- patches/lindroid.sh
+++ patches/lindroid.sh
@@ -9,8 +9,6 @@ LIB='services.jar'
 FILE="com/android/server/ExtconStateObserver.smali"
 REGEX='const-string v0, "NAME"\s*invoke-virtual {p2, v0}, Landroid\/os\/UEventObserver\$UEvent;->get\(Ljava\/lang\/String;\)Ljava\/lang\/String;\s*move-result-object (\w+)\s*\.line \d+'
 PATCH=$(cat << EOF
-    $(android_logcat $LIB"_bypass_ExtconStateObserver_NullName" | sed 's/[][\.*^$(){}?+|\/]/\\&/g')
-
     if-nez \2, :ExtconStateObserver_NameNotNull
 
     return-void
@@ -27,8 +25,6 @@ LIB='services.jar'
 FILE='com/android/server/WiredAccessoryManager$WiredAccessoryObserver.smali'
 REGEX='const-string \w+, "SWITCH_NAME"\s*invoke-virtual {\w+, \w+}, Landroid\/os\/UEventObserver\$UEvent;->get\(Ljava\/lang\/String;\)Ljava\/lang\/String;\s*move-result-object (\w+)\s*\.line \d+'
 PATCH=$(cat << EOF
-    $(android_logcat $LIB"_bypass_WiredAccessoryObserver_NullName" | sed 's/[][\.*^$(){}?+|\/]/\\&/g')
-
     if-nez \2, :WiredAccessoryObserver_NameNotNull
 
     return-void
